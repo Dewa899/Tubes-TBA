e841683d479a7bbe869d87cc52d1f81aa8f2e94d
Dewa899 <90895658+Dewa899@users.noreply.github.com>
2023-06-28 02:51:39 +0700

    Update parser_1.py

--- parser_1.py
+++ parser_1.py
@@ -1,61 +1,100 @@
+from lexical import lexical_analyzer
+
 class Parser:
     def __init__(self, tokens):
         self.tokens = tokens
         self.current_token = None
-        self.index = -1
+        self.index = 0
         self.error = False
 
-    def parse(self):
-        self.advance()
-        self.expression()
-
-        if not self.error and self.current_token is None:
-            print("Formula valid.")
+    def match(self, expected_token):
+        if self.current_token == expected_token:
+            self.consume()
         else:
-            print("Formula tidak valid.")
+            self.error = True
 
-    def advance(self):
+    def consume(self):
         self.index += 1
         if self.index < len(self.tokens):
             self.current_token = self.tokens[self.index]
         else:
             self.current_token = None
 
-    def expression(self):
+    def parse(self):
+        self.current_token = self.tokens[self.index]
+        self.operasi()
+
+        if self.current_token is not None:
+            self.error = True
+
+        if self.error:
+            return False
+        else:
+            return True
+
+    def operasi(self):
         self.term()
-        while self.current_token in [3, 4]:
-            self.advance()
+        self.operasi_loop()
+
+    def operasi_loop(self):
+        if self.current_token in [3, 4, 5, 6, 7]:
+            self.operator()
             self.term()
+            self.operasi_loop()
 
     def term(self):
-        self.factor()
-        while self.current_token in [5, 6, 7, 8]:
-            self.advance()
-            self.factor()
+        self.faktor()
+        self.term_loop()
 
-    def factor(self):
-        if self.current_token in [1, 2]:
-            self.advance()
+    def term_loop(self):
+        if self.current_token == 4:
+            self.operator_minus()
+            self.faktor()
+            self.term_loop()
+
+    def faktor(self):
+        if self.current_token in [1,2]:
+            self.operan()
         elif self.current_token == 9:
-            self.advance()
-            self.expression()
-            if self.current_token == 10:
-                self.advance()
-            else:
-                self.error = True
+            self.grup()
+        elif self.current_token == 8:
+            self.absolut()
         else:
             self.error = True
 
+    def operan(self):
+        self.match(self.current_token)
 
-# Contoh penggunaan
-# input_tokens = [1, 3, 1, 4, 2, 5, 1]
-# parser = Parser(input_tokens)
-# parser.parse()
+    def grup(self):
+        self.match(9)
+        self.operasi()
+        self.match(10)
 
-# input_tokens = [2, 6, 2, 7, 1, 8]
-# parser = Parser(input_tokens)
-# parser.parse()
+    def absolut(self):
+        self.match(8)
+        self.operasi()
+        self.match(8)
 
-# input_tokens = [9, 1, "error"]
-# parser = Parser(input_tokens)
-# parser.parse()
+    def operator(self):
+        if self.current_token in [3, 4, 5, 6, 7]:
+            self.match(self.current_token)
+        else:
+            self.error = True
+
+    def operator_minus(self):
+        self.match(4)
+
+    def operator_minus(self):
+        self.match(4)
+
+# Formula input
+#input_formula = "123 + 234 - 1.234 x | -576 |"
+
+# Analisis leksikal
+#output_tokens = lexical_analyzer(input_formula)
+#print("Input: ", input_formula)
+#print("Output (Lexical):", " ".join(str(token) for token in output_tokens))
+
+# Analisis sintaksis
+#parser = Parser(output_tokens)
+#print(parser.parse())
